c61cff47a651032a45d2bb6a315328a8a2d3e39a
Allan COSTES <allancts.compte@gmail.com>
2023-03-02 10:57:08 +0100

    add login

--- js/login.js
+++ js/login.js
@@ -2,42 +2,53 @@ $(document).ready(function(){
     var isPro = false;
     var url = 'loginPar';
     var tokenName = 'ecoservicesTokenPar';
-    var hrefUrl = "";
+    var hrefUrl = "D:/CoursG4/Projet 2/ecoservices/pages/particulier/cataloguePar.html";
 
     $("#loginPar").click(function() {
         isPro = false;
         url = 'loginPar';
         tokenName = 'ecoservicesTokenPar';
-        hrefUrl = "";
+        hrefUrl = "D:/CoursG4/Projet 2/ecoservices/pages/particulier/cataloguePar.html";
+        document.getElementById('info').textContent = "Particulier";
     });
     $("#loginPro").click(function() {
         isPro = true;
-        url = 'loginPrp';
+        url = 'loginPro';
         tokenName = 'ecoservicesTokenPro';
-        hrefUrl = "";
+        hrefUrl = "D:/CoursG4/Projet 2/ecoservices/pages/professionel/cataloguePro.html";
+        document.getElementById('info').textContent = "Professionel";
     });
 
     $("#butLogin").click(function() {
-        console.log("*** try to Login ***");
-
         const mail = document.getElementById('input-mail').value;
         const pass = document.getElementById('input-pass').value;
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                mail: mail,
-                mdp: pass
-            })
-        };
 
-        fetch(`http://localhost:3000/${url}`, requestOptions)
-        .then(res => {return res.json();})
-        .then(data => {
-            console.log(data.token);
-            localStorage.setItem(tokenName, JSON.stringify(data.token));
-            window.location.href = hrefUrl;
-        })
-        .catch(e => {console.log(e);})
+        if(mail != "" && pass != ""){
+            console.log("*** try to Login ***");
+            document.getElementById('error').textContent = "";
+
+            const requestOptions = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    mail: mail,
+                    mdp: pass
+                })
+            };
+            fetch(`http://localhost:3000/${url}`, requestOptions)
+            .then(res => {
+                const data = res.json();
+                if(res.status == 200){
+                    console.log(data.token);
+                    localStorage.setItem(tokenName, JSON.stringify(data.token));
+                    window.location.href = hrefUrl;
+                }
+                else if (res.status == '401'){document.getElementById('error').textContent = "Login ou mot de passe incorrect";}
+                else{document.getElementById('error').textContent = "error"}
+            })
+            .catch(e => {console.log("***  "+e+" ***");});
+        }
+        else{document.getElementById('error').textContent = "Veuillez remplir les champs";}
+
     });
   });
