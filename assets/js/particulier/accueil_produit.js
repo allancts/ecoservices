5ba4cfed323765c57be631ebcca4225657addf17
Allan COSTES <allancts.compte@gmail.com>
2023-03-03 10:03:13 +0100

    fix

---
+++ assets/js/particulier/accueil_produit.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    $("#butProfil").click(function() {
+        window.location.href = "../../pages/particulier/profilPar.html";
+    });
+    $("#butPanier").click(function() {
+        window.location.href = "../../pages/particulier/panierPar.html";
+    });
+    $("#butLogin").click(function() {
+        window.location.href = "../../pages/connexion.html";
+    });
+  });
---
+++ assets/js/professionel/accueil_produit_pro.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    $("#butProfil").click(function() {
+        window.location.href = "../../pages/professionel/profilPro.html";
+    });
+    $("#butPanier").click(function() {
+        window.location.href = "../../pages/professionel/panierPro.html";
+    });
+    $("#butLogin").click(function() {
+        window.location.href = "../../pages/connexion.html";
+    });
+  });
